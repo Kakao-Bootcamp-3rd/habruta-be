24ee411c307a8f3ffbd56a1bcf915d60d2470032
halo <dongjin778@naver.com>
2026-06-03 02:18:43 +0900

    perf: 멀티 인스턴스 환경에서 배치 동시 실행으로 인한 데드락을 제거하기 위해, ShedLock 기반 분산 락 도입

--- src/main/java/com/imyme/mine/MineApplication.java
+++ src/main/java/com/imyme/mine/MineApplication.java
@@ -1,5 +1,6 @@
 package com.imyme.mine;
 
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -8,6 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 @EnableAsync
+@EnableSchedulerLock(defaultLockAtMostFor = "PT10S")
 public class MineApplication {
 
   public static void main(String[] args) {
